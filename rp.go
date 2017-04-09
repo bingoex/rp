ba327cb9e20064cc4e38448959cb1a2d3477db12
bingoex <haibinwumr@163.com>
2017-04-09 22:59:53 +0800

    add introduct for rp

--- rp.go
+++ rp.go
@@ -19,13 +19,13 @@ import (
 
 var proxyAddr *string = flag.String("p", "127.0.0.1:8080", "proxy addr")
 var listenAddr *string = flag.String("l", ":80", "listen addr")
-var fileName = flag.String("f", "iptables.txt", "-f connip.txt")
+var fileName *string = flag.String("f", "iptables.txt", "-f connip.txt")
 
 var (
 	remote      *url.URL
 	remoteProxy *httputil.ReverseProxy
 
-	openLog  int64
+	openLog  int64 //日志开关
 	iptables []string
 )
 
@@ -49,6 +49,7 @@ func Logln(v ...interface{}) {
 	}
 }
 
+/* 初始化 */
 func init() {
 	flag.Int64Var(&openLog, "log", 1, "force open log")
 	flag.Parse()
@@ -60,12 +61,13 @@ func init() {
 		log.Fatal(err)
 	}
 
+	/* 后端代理 */
 	remoteProxy = httputil.NewSingleHostReverseProxy(remote)
 
 	fmt.Printf("proxying %s, listenning on %s\n\n", *proxyAddr, *listenAddr)
 }
 
-//http返回重定向
+/* 重定向 */
 func jumpTo(url string, w http.ResponseWriter) {
 	Logln("url:", url)
 	fmt.Fprintf(w, "<html><head><meta http-equiv=\"Refresh\" content=\"0;URL=%s\"></head><body></body></html>", url)
@@ -84,7 +86,7 @@ func dumpticket(ticket string) {
 }
 
 func getUserNameOA(ticket, ip string) (string, error) {
-	//调用服务器本地程序校验签名(票据)
+	/* 调用服务器本地程序校验签名(票据) */
 	c := exec.Command("/data/CheckOaKey", ticket, ip)
 	d, err := c.CombinedOutput()
 
@@ -96,7 +98,7 @@ func getUserNameOA(ticket, ip string) (string, error) {
 	return string(d), nil
 }
 
-// check if user in previleged list
+/* check if user in previleged list */
 func verifyUser(r *http.Request) (username string, errRet error) {
 	if r.Form.Get("ticket") == "" {
 		Logln("no ticket param")
@@ -113,7 +115,6 @@ func CheckUserPrevilige(user string) bool {
 }
 
 func CheckIpAndUrl(path string, url string) bool {
-
 	if path == "" || url == "" {
 		return false
 	}
@@ -132,15 +133,15 @@ func CheckIpAndUrl(path string, url string) bool {
 	return false
 }
 
+/* 获取白名单IP */
 func getIpTableLists() (iplist []string, err error) {
 
 	file, err := os.Open(*fileName)
+	defer file.Close()
 	if err != nil {
 		fmt.Println("Open file error")
 		return nil, errors.New("Open file error")
 	}
-
-	defer file.Close()
 
 	bfRd := bufio.NewReader(file)
 
@@ -154,6 +155,7 @@ func getIpTableLists() (iplist []string, err error) {
 			fmt.Println("ReaderBytes error")
 			break
 		}
+
 		Logln("iplist:", line)
 		iplist = append(iplist, line[:len(line)-1])
 	}
@@ -165,17 +167,16 @@ func showNoPrevilige(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "You do not have previlige, contacts administrator", 400)
 }
 
-//代理服务器类
+/* 服务器类 */
 type Proxy struct {
 	host string
 }
 
-//当有请求来时，则调用该接口
+/* http请求处理回调 */
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	orgURL := "http://" + *listenAddr + r.URL.Path
-
 	Logf("reqhost='%s', url='%s', orgURL='%s'\n", r.URL.Host, r.URL, orgURL)
 
 	var (
@@ -183,37 +184,34 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		err error
 	)
 
-	// iptables
+	/* 白名单校验 */
 	if CheckIpAndUrl(r.URL.Path, strings.Split(r.RemoteAddr, ":")[0]) {
 		Logln("checkUrlPrevilige ok  pass!!")
 		remoteProxy.ServeHTTP(w, r)
-
 		return
 	}
 
-	if lp, err = r.Cookie("loginParam"); err != nil || lp.Value == "" { // no cookie, then jump to oa
-		// 没有登录，去登录服务器校验
+	/*  TODO 此逻辑根据用户需要修改 */
+	if lp, err = r.Cookie("loginParam"); err != nil || lp.Value == "" {
+		/* 没有登录，去登录服务器校验 */
 		if r.Form.Get("ticket") == "" {
 			jumpTo(makeOALoginURL(orgURL), w)
-
 			return
 		}
 
-		// 来到这里,证明刚从登录服务器验证后跳转过来的
+		/* 来到这里,证明刚从登录服务器验证后跳转过来的 */
 		if _, err := verifyUser(r); err != nil {
 			http.Error(w, "TOF's ticket is invalidate, try refresh again", 400)
-
 			return
 		}
 
-		// set cookie and jump by cookie again
+		/* set cookie and jump with cookie again */
 		w.Header().Set("Set-Cookie", fmt.Sprintf("loginParam=%s;path=/;", r.Form.Get("ticket")))
 		jumpTo(orgURL, w)
 
 		return
-
 	} else {
-		//来到这里，证明已经登录过，而且有了cookie里有签名（票据）
+		/* 来到这里，证明已经登录过，而且有了cookie里有签名（票据）*/
 		var (
 			user string
 			err  error
@@ -222,39 +220,35 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		clientIP := strings.Split(r.RemoteAddr, ":")[0]
 
 		if user, err = getUserNameOA(lp.Value, clientIP); err != nil {
-
 			jumpTo(makeOALoginURL(orgURL), w)
-
 			return
 		} else if user == "" {
 			//BUG
 			Logln("user is empty")
 			jumpTo(makeOALoginURL(orgURL), w)
-
 			return
 		}
 
-		//校验用户权限,目前什么都没做
+		/* 校验用户权限,目前什么都没做 */
 		if !CheckUserPrevilige(user) {
 			showNoPrevilige(w, r)
-
 			return
 		}
 	}
 
-	//调用系统的代理服务器类,透出请求
+	/* 调用系统的代理服务器类,透出请求 */
 	remoteProxy.ServeHTTP(w, r)
 }
 
 func main() {
-
-	//获取白名单IP
+	/* 获取白名单IP */
 	var err error
 	iptables, err = getIpTableLists()
 	if err != nil {
 		Logln("getIpTableLists false error : ", err)
 	}
 
+	/* 启动proxy */
 	p := &Proxy{host: *proxyAddr}
 	log.Fatal(http.ListenAndServe(*listenAddr, p))
 }
